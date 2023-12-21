4c27421364d2d62fe32e6d3b28dcd2ebe1254278
Sawaraa <daniellakotlar@gmail.com>
2023-12-21 21:13:48 +0200

    Initial commit

---
+++ src/Min/Main.java
@@ -0,0 +1,23 @@
+import Min.Collection;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Car car = new Car("ламборджіні", 24);
+//
+//        Car.Engeen eng = car.new Engeen();
+//
+//        Car.A a = new Car.A();
+//
+//        eng.start();
+//        a.start();
+
+        Number[] array = {1, 2, 3, 4, 5, 6, 7};
+
+        Collection collection = new Collection(array);
+
+        Collection.First firstIterator = collection.new First();
+
+
+    }
+}
